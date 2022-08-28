b16c69923024fb40045250eeb518ea62c7b4e5a7
Andres Diaz <andiazo@unal.edu.co>
2022-08-28 14:41:41 -0500

    Updating style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
 const Footer = () => {
-	return (
-		<footer>
-			<p className='text-yellow-500 text-center self-end w-full py-1'>
-				IA:https://github.com/cysmith/neural-style-tf Developers: Aitor Zaldua,
-				Santiago Somoza, Renny Petit, Anas Bouhanassa, Andrés Díaz{' '}
-			</p>
-		</footer>
-	);
+    return (
+        <footer>
+            <p className="text-white text-center self-end w-full py-1">
+                <span className="text-yellow-500">IA: </span>{' '}
+                https://github.com/cysmith/neural-style-tf
+                <span className="text-yellow-500"> Developers: </span> Aitor
+                Zaldua, Santiago Somoza, Renny Petit, Anas Bouhanassa, Andrés
+                Díaz{' '}
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
