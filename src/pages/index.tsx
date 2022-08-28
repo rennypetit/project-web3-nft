99df1282440191bd6ee743d16978a22b32687dc2
rennypetit <rennypetit01@gmail.com>
2022-08-27 21:51:01 -0500

    Update: layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from 'next';
 import ContentLeft from '../components/ContentLeft';
 import ContentRight from '../components/ContentRight';
-
-import Layout from '../layout';
 const Home: NextPage = () => {
 	return (
-		<Layout>
-			<div className='w-11/12 grid grid-rows-1 gap-8 text-white p-0 pt-4 m-auto md:grid-cols-2'>
-				<ContentLeft />
-				<ContentRight />
-			</div>
-		</Layout>
+		<div className='w-11/12 grid grid-rows-1 gap-8 text-white p-0 pt-4 m-auto md:grid-cols-2'>
+			<ContentLeft />
+			<ContentRight />
+		</div>
 	);
 };
 
