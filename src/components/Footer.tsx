31c038019f07faf4c9ac2dc6aa48b020f35cc504
rennypetit <rennypetit01@gmail.com>
2022-08-27 21:42:36 -0500

    Add: layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+		<footer>
+			<p className='text-yellow-500 text-center self-end w-full py-1'>
+				IA:https://github.com/cysmith/neural-style-tf Developers: Aitor Zaldua,
+				Santiago Somoza, Renny Petit, Anas Bouhanassa, Andrés Díaz{' '}
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
-			{children}
+			<div className='Layout flex min-h-screen flex-wrap w-full'>
+				{children}
+			</div>
+			<Footer />
 		</>
 	);
 };
