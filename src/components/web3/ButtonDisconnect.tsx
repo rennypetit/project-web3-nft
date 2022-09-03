66ebd6e848f3ce9fe77de8165547c76437109016
rennypetit <rennypetit01@gmail.com>
2022-09-03 12:09:14 -0500

    Add: truncate address

--- src/components/web3/ButtonDisconnect.tsx
+++ src/components/web3/ButtonDisconnect.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import useTruncatedAddress from 'hooks/useTruncatedAddress';
 
 const ButtonDisconnect = ({ disconnect, account }) => {
+	const truncatedAddress = useTruncatedAddress(account);
 	return (
 		<button
 			onClick={disconnect}
 			className='font-rubik font-normal whitespace-nowrap inline-flex items-center justify-center px-4 py-2  rounded-md shadow-sm text-basetext-yellow-500 bg-transparent relative w-28 m-0'
 		>
-			<span className='p-0.5 font-medium text-yellow-500'>Hola {account}</span>
+			<span className='p-0.5 font-medium text-yellow-500'>
+				Hola {truncatedAddress}
+			</span>
 			<div className='rounded-md shadow-sm text-base bg-transparent absolute w-full h-full p-4 top-0 left-0 m-1'></div>
 		</button>
 	);
