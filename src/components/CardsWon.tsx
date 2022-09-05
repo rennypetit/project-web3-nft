46ad20fbfbee7ec1a23ea1838693e30c2ba4a9aa
andiazo <andiazo@unal.edu.co>
2022-09-04 21:42:18 -0500

    Fixing cards

--- src/components/CardsWon.tsx
+++ src/components/CardsWon.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from 'next/image';
 
-const CardsWon = () => {
-  return (
-    <div>CardsWon</div>
-  )
-}
+const CardsWon = (props: any) => {
+	return (
+		<>
+			<div className='Card xxs:mx-8'>
+				<Image
+					src='/images/nft.png'
+					alt='nft'
+					width='319'
+					height='291'
+					layout='responsive'
+				/>
+				<p className='text-center pt-4 font-poppins font-medium'>{props.name}</p>
+				<p>Price: {props.price} ETH</p>
+                <p>Participants: {props.ParNum}</p>
+				<p>Winner: {props.WinName}</p>
+			</div>
+		</>
+	);
+};
 
-export default CardsWon
+export default CardsWon;
