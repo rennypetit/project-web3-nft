d111454e17c5c13a9219c85b75580cce8bf2e506
andiazo <andiazo@unal.edu.co>
2022-09-04 23:20:48 -0500

    Updating ui

--- src/components/LotteryCard.tsx
+++ src/components/LotteryCard.tsx
@@ -10,6 +10,10 @@ const LotteryCard = (props: any) => {
     const dateString = newDate.toUTCString();
     // 
 
+    const handleSelect = (event: any) => {
+        event.preventDefault();
+    }
+
     return (
         <div>
             <p>Owner: {props.lotto.nftOwner}</p>
@@ -23,6 +27,11 @@ const LotteryCard = (props: any) => {
                 <p>Winner: {props.lotto.lotteryWinner}</p>
             }
             <p>End date: {dateString}</p>
+            <button className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'>
+                <span className='relative px-5 py-2.5 transition-all font-bold ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                    Select this
+                </span>
+            </button>
         </div>
     )
 }
