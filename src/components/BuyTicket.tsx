42c176bb754272291879d79eca23ae92b49e8164
andiazo <andiazo@unal.edu.co>
2022-09-04 23:06:59 -0500

    Integrating get Lottery method

--- src/components/BuyTicket.tsx
+++ src/components/BuyTicket.tsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import Services from '@services/index';
+import { useWeb3React } from '@web3-react/core';
+import { useLottery, useNFT } from 'hooks';
+import React, { useCallback, useEffect, useState } from 'react'
+import LotteryCard from './LotteryCard';
 
 const BuyTicket = () => {
+
+    const services = new Services(); // instance
+    const lottery = useLottery();
+    const nft = useNFT();
+    const { active, account } = useWeb3React();
+    const [data, setData] = useState(null);
+    const [activeLotteries, setActiveLotteries] = useState([{}]);
+
+    const getData = useCallback(async () => {
+        if (lottery && nft) {
+            // l.nftOwner, l.nftContractAddress, l.bettingPrice, l.activeLottery, l.players, l.lotteryBalance, l.lotteryWinner, l.endDate
+            const response = await services.methodGetLottery(lottery);
+            if (response) {
+                const nftOwner = response[0]
+                const nftContractAddress = response[1]
+                const bettingPrice = response[2]
+                const activeLottery = response[3]
+                const players = response[4]
+                const lotteryBalance = response[5]
+                const lotteryWinner = response[6]
+                const endDate = response[7]
+                const newLotto = { nftOwner, nftContractAddress, bettingPrice, activeLottery, players, lotteryBalance, lotteryWinner, endDate }
+                setActiveLotteries([...activeLotteries, newLotto])
+            }
+            console.log(response);
+        }
+    }, [activeLotteries, lottery, nft, services]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <>
             <h2 className='font-josef text-1xl m-0 pt-0 lg:text-2xl text-orange-500'>
@@ -14,6 +50,9 @@ const BuyTicket = () => {
                     <h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>
                         CHOOSE
                     </h1>
+                    <p>
+                    {activeLotteries[1] ? <LotteryCard lotto={activeLotteries[1]}/> : 'There is not lotteries'}
+                    </p>
                 </div>
                 <div className='bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-1/3 m-2 p-4 sm:w-full xxs:w-full'>
                     <h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>
@@ -23,7 +62,7 @@ const BuyTicket = () => {
                         <span className=' px-5 py-2.5 transition-all font-bold ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
                             Buy ticket
                         </span>
-                        
+
                     </button>
                 </div>
                 <div className='bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-1/3 m-2 p-4 sm:w-full xxs:w-full'>
---
+++ src/components/LotteryCard.tsx
@@ -0,0 +1,30 @@
+import { useWeb3React } from '@web3-react/core'
+import React, { useEffect } from 'react'
+
+const LotteryCard = (props: any) => {
+    // const newLotto = { nftOwner, nftContractAddress, bettingPrice, activeLottery, players, lotteryBalance, lotteryWinner, endDate }
+    const { library } = useWeb3React();
+    let bettingPrice = library.utils.fromWei(props.lotto.bettingPrice.toString(), 'ether')
+    const newDate = new Date();
+    newDate.setTime(props.lotto.endDate * 1000);
+    const dateString = newDate.toUTCString();
+    // 
+
+    return (
+        <div>
+            <p>Owner: {props.lotto.nftOwner}</p>
+            <p>Nft contract: {props.lotto.nftContractAddress}</p>
+            <p>Betting price: {bettingPrice} eth</p>
+            <p>Is active: {props.lotto.activeLottery ? 'true' : 'false'}</p>
+            <p>Players: {props.lotto.players.length}</p>
+            <p>Balance: {props.lotto.lotteryBalance}</p>
+            {props.lotto.lotteryWinner === '0x0000000000000000000000000000000000000000' ?
+                <p>Winner: No winner </p> :
+                <p>Winner: {props.lotto.lotteryWinner}</p>
+            }
+            <p>End date: {dateString}</p>
+        </div>
+    )
+}
+
+export default LotteryCard
