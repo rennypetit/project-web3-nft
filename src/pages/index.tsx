406a005f2f8a0b0986b5844ca929f915e8024ab3
anasklm <105330623+anasklm@users.noreply.github.com>
2022-09-06 11:11:54 +0100

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,29 @@ const Home: NextPage = () => {
 			<StartLottery />
 			<ContentLeft />
 			
-			<div className='w-1/2 mx-auto xl:w-1/2 md:w-4/5 xxs:w-4/5'>
-				<h1 className='md:text-4xl xxs:text-5xl text-2xl font-josef font-bold  mt-10 mb-5'>Current Giveaways</h1>
-				<div className='flex mx-auto justify-between flex-wrap'>
-					{items.map((item, index) => (
-						<Card key={index} name={'Pikatchu'} price={0.1} number={10} end={5}/>
-					))}
-				</div>
+			<div className='w-1/2 mx-auto xl:w-4/5 md:w-4/5 xxs:w-4/5 mt-10'>
+				<div className='w-11/12  text-white p-0 m-auto'>
+							<h2 className='font-josef text-1xl m-0 pt-0 lg:text-2xl text-orange-500'>Need to know about</h2>
+							<h1 className='text-2xl font-josef font-bold leading-1 md:text-6xl xxs:text-5xl'>HOW TO PLAY</h1>
+							<h3 className='font-josef text-1xl m-0 pt-0 lg:text-2xl mb-5'>Follow these 3 easy steps!</h3>
+							<div className="flex flex-wrap justify-around m-auto  mb-7">
+								<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-1/4 m-2 p-4 sm:w-1/2 xxs:w-full">
+									<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>CHOOSE</h1>
+									<img src="/images/1.png" alt="" className='mx-auto mb-10'/>
+									<p className='text-center'>Register to RIFA & Choose your contest</p>
+								</div>
+								<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-1/4 m-2 p-4 sm:w-1/2 xxs:w-full">
+									<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>BUY</h1>
+									<img src="/images/2.png" alt="" className='mx-auto mb-10'/>
+									<p className='text-center'>Pick Your Numbers & Complete your Purchase</p>
+								</div>
+								<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-1/4 m-2 p-4 sm:w-1/2 xxs:w-full">
+									<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>WIN</h1>
+									<img src="/images/3.png" alt="" className='mx-auto mb-10'/>
+									<p className='text-center'>Start Dreaming, you're almost there</p>
+								</div>
+							</div>
+						</div>
 			</div>
 		</div>
 	);
--- src/pages/lottery.tsx
+++ src/pages/lottery.tsx
@@ -7,26 +7,10 @@ const Lottery: NextPage = () => {
 		<>
 		<StartLottery />
 		<div className='w-11/12  text-white p-0 m-auto'>
-			<h2 className='font-josef text-1xl m-0 pt-0 lg:text-2xl text-orange-500'>Need to know about</h2>
-			<h1 className='text-2xl font-josef font-bold leading-1 md:text-8xl xxs:text-5xl'>HOW TO PLAY</h1>
-			<h3 className='font-josef text-1xl m-0 pt-0 lg:text-2xl'>Follow these 3 easy steps!</h3>
-			<div className="flex flex-wrap justify-around xl:mx-28 md:mx-auto sm:mx-auto xxs:mx-auto mb-7">
-				<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-fit m-2 p-4 sm:w-1/2 xxs:w-full">
-					<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>CHOOSE</h1>
-					<img src="/images/1.png" alt="" className='mx-auto mb-10'/>
-					<p className='text-center'>Register to RIFA & Choose your contest</p>
-				</div>
-				<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-fit m-2 p-4 sm:w-1/2 xxs:w-full">
-					<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>BUY</h1>
-					<img src="/images/2.png" alt="" className='mx-auto mb-10'/>
-					<p className='text-center'>Pick Your Numbers & Complete your Purchase</p>
-				</div>
-				<div className="bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 rounded-3xl  md:w-fit m-2 p-4 sm:w-1/2 xxs:w-full">
-					<h1 className='mb-5 text-center font-josef text-1xl m-0 pt-0 lg:text-2xl'>WIN</h1>
-					<img src="/images/3.png" alt="" className='mx-auto mb-10'/>
-					<p className='text-center'>Start Dreaming, you're almost there</p>
-				</div>
-			</div>
+			
+			<h1 className='text-2xl font-josef font-bold leading-1 md:text-8xl xxs:text-5xl mb-10'>PLAY NOW!</h1>
+			
+			
 			<div className='container-card-collection w-4/5 z-10'>
 				{items.map((item, index) => (
 					<Card key={index} name={'Pikatchu'} price={0.1} number={10} end={5}/>
