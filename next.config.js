fa23b6772f4cd481e419ed2bd23bb95120fbbd50
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-09-12 20:17:50 -0300

    Added workaround to fix fs error and env variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config;
+    },
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
@@ -11,5 +22,9 @@ module.exports = {
 			'raw.githubusercontent.com',
 			'avatars.githubusercontent.com'
 		]
+	},
+	env: {
+		ETHEREUM_NETWORK: process.env.ETHEREUM_NETWORK,
+		INFURA_API_KEY: process.env.INFURA_API_KEY
 	}
 };
