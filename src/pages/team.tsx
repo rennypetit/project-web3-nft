eb29c5ebc5e2ca9a7bc815c08d930bbd228756a3
anasklm <anas.bhh12@gmail.com>
2022-09-13 11:44:55 +0100

    edited Team page styling

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -8,8 +8,7 @@ const Team: NextPage = () => {
         <>
 		<div className='w-11/12  text-white p-0 m-auto'>
             <h1 className='text-2xl text-white text-center font-josef font-bold leading-1 md:text-6xl xxs:text-5xl mb-10'>
-				Development 
-				<span className='text-yellow-500'> Team</span> <br /> <br />
+				Development Team <br /> <br />
 			</h1>
 			<div className='container-card-collection'>
                 {items.map((item, index) => (
